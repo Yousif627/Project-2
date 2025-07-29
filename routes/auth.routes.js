61e90915f012ab34753f8918fce76916aebd6886
Yousif627 <yousifabdulaziz18@gmail.com>
2025-07-29 02:46:42 -0700

    add the post for the username and password and if the user already exist in the data ornot and added a limit fo teh password length

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,7 +9,36 @@ router.get("/sign-up", (req,res)=>{
 })
 
 router.post("/sign-up", async (req,res)=>{
-    try
+    try{
+        const { username, password} = req.body;
+
+
+        if (!username || !password){
+            return res.render("auth/sign-up",{
+                error:"All fields are required."
+            });
+        }
+
+            if (password.length < 8) {
+            return res.render("auth/sign-up", {
+                error: "Password must be at least 6 characters long."
+            });
+        }
+
+           const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.render("auth/sign-up", {
+                error: "Username is already taken."
+
+            });
+        }
+
+
+
+    }
+    catch(error){
+        console.log("error ahs appeared ")
+    }
 
 
 })
